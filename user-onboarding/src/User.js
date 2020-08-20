fac88ce30794a12b1c42885b62d216c5c5e391f3
Tom Bauer <tbauer1898@gmail.com>
2020-08-20 10:17:58 -0500

    Added minor styling.

--- user-onboarding/src/User.js
+++ user-onboarding/src/User.js
@@ -10,7 +10,7 @@ export default function User({details}) {
       }
     
       return (
-        <div className='friend container'>
+        <div className='user-div'>
           <h2>{details.first_name} {details.last_name}</h2>
           <h2>{details.username}</h2>
           <p>Email: {details.email}</p>
